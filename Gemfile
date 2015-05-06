4942d164e03b912c3fc4aa0ab45f2ec817595c82
Michał Kurek <oki@md6.org>
2015-05-06 08:04:12 +0200

    Init commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "forecast_io"
+gem "dotenv"
+gem "pry"
---
+++ show_my_weather.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "bundler/setup"
+require "pp"
+require "open-uri"
+require "json"
+
+Bundler.require
+Dotenv.load
+
+ForecastIO.api_key = ENV.fetch('FORECAST_KEY')
+ForecastIO.default_params = { units: 'si' }
+
+# http://unicode-table.com/en/blocks/miscellaneous-symbols-and-pictographs/
+
+begin
+  # geo_data = JSON.load(open('http://freegeoip.net/json/'))
+  geo_data = JSON.load(open("http://ip-api.com/json"))
+  # pp geo_data
+
+  forecast = ForecastIO.forecast(geo_data["lat"], geo_data["lon"], time: Time.now.to_i)
+
+  icon = forecast.currently["icon"] rescue '☹'
+
+  # clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+  puts case icon
+  when "clear-day"
+    # '☀'
+    '🔆'
+  when "clear-night"
+    '⋆⭑'
+  when "rain"
+     '☂'
+    #'🌂'
+  when "snow"
+    '❄'
+  when "sleet"
+    '*"'
+  when "wind"
+    #'⚑'
+    #"🎌"
+    "🏁"
+  when "fog"
+    '☁'
+  when "cloudy"
+    '☁'
+  when "partly-cloudy-day"
+    '☁☀'
+  when "partly-cloudy-night"
+    '☁'
+  else
+    '☢'
+  end
+
+rescue => e
+  if STDIN.tty?
+    puts e
+  end
+  puts '☹'
+end
